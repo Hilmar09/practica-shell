80464c99df4fbceda3d3cfd3bdeb798cf0730038
Hilmar09 <hil.cris09@gmail.com>
2024-05-20 09:47:13 +0200

    prueba de rama

---
+++ sustitucion_de_comandos.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+current_year=$(date +%Y)
+current_month=$(date +%m)
+current_day=$(date +%d)
+
+birth_year=1992
+birth_month=05
+birth_day=09
+
+age=$((current_year - birth_year))
+echo "tienes $age años"
+
+
+
+
+
+
